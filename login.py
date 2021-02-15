7baf7720fd017109fd7cecbe3a21f3bd9b94f4f5
smmnaghibi <naghibi.smm@gmail.com>
2021-02-15 11:08:05 +0330

    building sign in window

--- login.py
+++ login.py
@@ -8,7 +8,18 @@ from tkinter import *
 # login = Tk()
 # Button(login, text="Create new window", command=create_window).grid(row=0, column=0)
 def sign_in():
-    ...
+    def check():
+        ...
+    sign_in_window = Tk()
+    sign_in_window.title("Sign in")
+    Label(sign_in_window, text="username : ").grid(row=0, column=0)
+    uname = Entry(sign_in_window)
+    uname.grid(row=0, column=1)
+    Label(sign_in_window, text="password : ").grid(row=1, column=0)
+    password = Entry(sign_in_window)
+    password.grid(row=1, column=1)
+    Button(sign_in_window, text="              log in              ", command=check).grid(row=2,column=1)
+    sign_in_up_window.destroy()
 
 def sign_up():
     ...
