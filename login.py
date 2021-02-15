dc9157e63b561d35a873024ebd1346efb2c822a7
smmnaghibi <naghibi.smm@gmail.com>
2021-02-15 11:11:17 +0330

    building sign up window

--- login.py
+++ login.py
@@ -18,11 +18,23 @@ def sign_in():
     Label(sign_in_window, text="password : ").grid(row=1, column=0)
     password = Entry(sign_in_window)
     password.grid(row=1, column=1)
-    Button(sign_in_window, text="              log in              ", command=check).grid(row=2,column=1)
+    Button(sign_in_window, text="              sign in              ", command=check).grid(row=2,column=1)
     sign_in_up_window.destroy()
 
 def sign_up():
-    ...
+    def check():
+        ...
+
+    sign_up_window = Tk()
+    sign_up_window.title("Sign up")
+    Label(sign_up_window, text="username : ").grid(row=0, column=0)
+    uname = Entry(sign_up_window)
+    uname.grid(row=0, column=1)
+    Label(sign_up_window, text="password : ").grid(row=1, column=0)
+    password = Entry(sign_up_window)
+    password.grid(row=1, column=1)
+    Button(sign_up_window, text="             sign up             ", command=check).grid(row=2, column=1)
+    sign_in_up_window.destroy()
 
 sign_in_up_window = Tk()
 sign_in_up_window.title("Login")
